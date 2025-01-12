eab473f4aeb78839dc6b016a523de372e183310b
allengmcd <alleng.mcd@gmail.com>
2025-01-12 17:39:33 -0600

    assignment 2

---
+++ finder-app/writer.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include<syslog.h>
+
+int main(int argc, char **argv)
+{
+    openlog("Logs", LOG_PID | LOG_CONS, LOG_USER);
+    syslog(LOG_INFO, "Starting program...\n");
+
+    if(argc != 3)
+    {
+        printf("Must have 2 arguments...\n");
+        syslog(LOG_ERR, "Must have 2 arguments...\n");
+        return 1;
+    }
+
+    char* writefile = argv[1];
+    char* writestr = argv[2];
+
+    FILE *file;
+
+    file = fopen(writefile, "w");
+    if (file == NULL) {
+        syslog(LOG_ERR,"Error opening file...");
+        return 1; // Exit or handle the error
+    }
+
+    syslog(LOG_DEBUG, "Writing %s to %s", writestr, writestr);
+    if (fprintf(file, "%s", writestr) < 0) {
+        syslog(LOG_ERR,"Error writing to file...");
+        fclose(file);
+        return 1; // Exit or handle the error
+    }
+
+    if (fclose(file) == EOF) {
+        syslog(LOG_ERR,"Error closing file...");
+        return 1; // Exit or handle the error
+    }
+
+    return 0;
+}
